c759e77eaa87bd51aaf07bf02b30ab3d3e69e7da
4419-3-7\user <torako0510@gmail.com>
2017-11-25 14:53:57 +0800

    1125 CLASS WORK

--- Assets/Scripts/ShootingGalleryController.cs
+++ Assets/Scripts/ShootingGalleryController.cs
@@ -3,8 +3,101 @@ using System.Collections.Generic;
 using UnityEngine;
 using VRStandardAssets.Common;
 using VRStandardAssets.Utils;
-using UnityEngine.UI;
-public class ShootingGalleryController : MonoBehaviour
+using UnityEngine.UI;//Namespace 命名空間
+public class ShootingGalleryController : MonoBehaviour//Class 類別
 {
+    public UIController uiController;//Field 欄位
+    public Reticle reticle;
+    public SelectionRadial selectionRadial;
+    public SelectionSlider selectionSlider;
 
+    public Image timerBar;
+    public float gameDuration = 30f;
+    public float endDelay = 1.5f;
+
+
+    public Collider spawnCollider;
+    public ObjectPool targetObjectPool;
+    public float spawnProbabilty = 0.7f;
+    public float spawnInterval = 1f;
+    private float spawnTimer;
+
+    public bool IsPlaying//Property 屬性
+    {
+        private set;
+        get;
+    }
+    private IEnumerator Start()//Method 方法
+    {
+        SessionData.SetGameType(SessionData.GameType.SHOOTER180);
+        while(true)
+        {
+            Debug.Log("Start StartPhase");
+            yield return StartCoroutine(StartPhase());
+            Debug.Log("Start PlayPhase");
+            yield return StartCoroutine(PlayPhase());
+            Debug.Log("Start EndPhase");
+            yield return StartCoroutine(EndPhase());
+            Debug.Log("Complete");
+        }
+    }
+
+    private IEnumerator StartPhase()
+    {
+        yield return StartCoroutine(uiController.ShowIntroUI());
+        reticle.Show();
+        selectionRadial.Hide();
+        yield return StartCoroutine(selectionSlider.WaitForBarToFill());
+        yield return StartCoroutine(uiController.HideIntroUI());
+    }
+
+    private IEnumerator PlayPhase()
+    {
+        yield return StartCoroutine(uiController.ShowPlayerUI());
+        IsPlaying = true;
+        reticle.Show();
+        SessionData.Restart();
+        float gameTimer = gameDuration;
+        while(gameTimer > 0f)
+        {
+            if (spawnTimer <= 0f)
+            {
+                if(Random.value < spawnProbabilty)
+                {
+                    spawnTimer = spawnInterval;
+                    Spawn();
+                }
+            }
+            yield return null;
+            gameTimer -= Time.deltaTime;
+            spawnTimer -= Time.deltaTime;
+            timerBar.fillAmount = gameTimer / gameDuration;
+        }
+        IsPlaying = false;
+        yield return StartCoroutine(uiController.HidePlayerUI());
+    }
+
+    private void Spawn()
+    {
+        GameObject target = targetObjectPool.GetGameObjectFromPool();
+        target.transform.position = SpawnPosition();
+    }
+    private Vector3 SpawnPosition()
+    {
+        Vector3 center = spawnCollider.bounds.center;
+        Vector3 extents = spawnCollider.bounds.extents;
+        float x = Random.Range(center.x - extents.x, center.x + extents.x);
+        float y = Random.Range(center.y - extents.y, center.y + extents.y);
+        float z = Random.Range(center.z - extents.z, center.z + extents.z);
+        return new Vector3(x, y, z);
+    }
+
+    private IEnumerator EndPhase()
+    {
+        reticle.Hide();
+        yield return StartCoroutine(uiController.ShowOutroUI());
+        yield return new WaitForSeconds(endDelay);
+        yield return StartCoroutine(selectionRadial.WaitForSelectionRadialToFill());
+        yield return StartCoroutine(uiController.HideOutroUI());
+    }
 }
--- Assets/Scripts/ShootingGunController.cs
+++ Assets/Scripts/ShootingGunController.cs
@@ -5,5 +5,69 @@ using VRStandardAssets.Utils;
 using UnityEngine.VR;
 public class ShootingGunController : MonoBehaviour
 {
+    public AudioSource audioSource; 
+    public VRInput vrInput;
+    public ParticleSystem flareParticle;
+    public LineRenderer gunFlare;
+    public Transform gunEnd;
+    //----------------------------------------
+    public Transform cameraTransform;
+    public Reticle reticle;
+    public Transform gunContainer;
+    public float damping = 0.5f;
+    public float dampingCoef = -20f;
+    public float gunContainerSmoothe = 10f;
+    //----------------------------------------
+    public float defaultLineLength = 70f;
+    public float gunFlareVisibleDeconds = 0.07f;
+    private void OnEnable()
+    {
+        vrInput.OnDown += HandleDown;
+    }
+
+    private void OnDisable()
+    {
+        vrInput.OnDown -= HandleDown;
+    }
+    private void HandleDown()
+    {
+        
+        StartCoroutine(Fire());
+        
+    }
+
+    private IEnumerator Fire()
+    {
+        audioSource.Play();
+        float lineLength = defaultLineLength;
+        //TODO 判斷有無射到東西
+        flareParticle.Play();
+        gunFlare.enabled = true;
+        yield return StartCoroutine(MoveLineRenderer(lineLength));
+        gunFlare.enabled = false;
+    }
+
+    private IEnumerator MoveLineRenderer(float lineLength)
+    {
+        float timer = 0f;
+        while(timer <gunFlareVisibleDeconds)
+        {
+            gunFlare.SetPosition(0, gunEnd.position);
+            gunFlare.SetPosition(1, gunEnd.position + gunEnd.forward * lineLength);
+            yield return null;
+            timer += Time.deltaTime;
+        } 
+    }
+
+    private void Update()
+    {
+        transform.rotation = Quaternion.Slerp(transform.rotation, InputTracking.GetLocalRotation(VRNode.Head), damping * (1 - Mathf.Exp(dampingCoef * Time.deltaTime)));
+        transform.position = cameraTransform.position;
+        Quaternion lookAtRotation = Quaternion.LookRotation(reticle.ReticleTransform.position - gunContainer.position);
+        gunContainer.rotation = Quaternion.Slerp(gunContainer.rotation, lookAtRotation, gunContainerSmoothe * Time.deltaTime);
+    }
+
 
 }
+
+
